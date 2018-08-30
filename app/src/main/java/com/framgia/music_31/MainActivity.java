f92235ed40e8dcb6805e146ca887e4fbd2541708
havanhung <havanhung0402@gmail.com>
2018-08-30 09:18:35 +0700

    ui mainactivity

--- app/src/main/java/com/framgia/music_31/MainActivity.java
+++ app/src/main/java/com/framgia/music_31/MainActivity.java
@@ -2,6 +2,10 @@ package com.framgia.music_31;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.menu_main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.menu_color:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
